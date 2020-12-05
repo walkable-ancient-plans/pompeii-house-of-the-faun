81e62ff314ecee352183ebf39b8315ba61535319
Sebastian Heath <sebastian.heath@gmail.com>
2020-12-05 11:56:07 -0500

    first

---
+++ README.md
@@ -0,0 +1,1 @@
+Multi-player "walkable" plan of the House of the Faunt at Pompeii. This repo can be downloaded and run on a local server.
---
+++ __settings__.js
@@ -0,0 +1,21 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "666443.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': true,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+SCRIPTS = [ 15551612, 15551618, 15551616, 15663310, 15675094, 16054524, 16080310 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+];
